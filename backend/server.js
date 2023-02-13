b32226b8d120a97e93cd2dda81d42a8c13b450e9
Ian Hart <ianhart13@gmail.com>
2023-02-13 14:43:09 -0500

    established mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+mongoose.set('strictQuery', false);
+
 require('dotenv').config();
 
 const app = express();
@@ -9,6 +11,15 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+const mongoDB = uri;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+  console.log('Successfully connected to MongoDB');
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port : ${port}`);
